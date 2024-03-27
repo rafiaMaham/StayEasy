d2add7981c285d6ee0196c72267f835c22e3fa93
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-27 18:25:03 +0530

    cors setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth"
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION as string);
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
