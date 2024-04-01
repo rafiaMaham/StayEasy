39e9f0933c1bd4dad34b7497fd099af931e16f11
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-02 02:28:20 +0530

    myhotelRoutes setup and functionality

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 import cloudinary from "cloudinary";
+import Hotel, { HotelType } from "../models/hotel.model";
+import verifyToken from "../middleware/auth";
+import { body } from "express-validator";
 
 const router = express.Router();
 
@@ -15,13 +18,29 @@ const upload = multer({
 //api/my-hotels
 router.post(
   "/",
+  verifyToken,
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    body("type").notEmpty().withMessage("Hotel type is required"),
+    body("pricePerNight")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price per night is required and must be a number"),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities are required"),
+  ],
   upload.array("imageFiles", 6),
   async (req: Request, res: Response) => {
     try {
       const imageFiles = req.files as Express.Multer.File[];
-      const newHotel = req.body;
+      const newHotel: HotelType = req.body;
 
-      //*upload the images to cloudinary
+      //1. upload the images to cloudinary
       const uploadPromises = imageFiles.map(async (image) => {
         const b64 = Buffer.from(image.buffer).toString("base64"); //convert image to base64 string
         let dataURI = "data:" + image.mimetype + ";base64," + b64; //kind of image to be uploaded like png, jpeg, etc
@@ -31,14 +50,23 @@ router.post(
 
       const imageUrls = await Promise.all(uploadPromises);
 
-      //* if the upload is successful then add the URLs to the new hotel
-      
+      //2. If the upload is successful then add the URLs to the new hotel
 
-      //*save the new hotel in our database
-      //* return a 201 status
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
+
+      //3. save the new hotel in our database
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
+
+      //4 return a 201 status
+      res.status(201).send(hotel);
     } catch (error) {
       console.log("Error creating hotel", error);
-      res.status(500).json({message: "Something went wrong"})
+      res.status(500).json({ message: "Something went wrong" });
     }
   }
 );
+
+export default router;
