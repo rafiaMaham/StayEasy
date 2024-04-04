1cfce0b98f559b5d26b740cc8c092264c976b930
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-04 15:29:05 +0530

    fetchMyHotels

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,8 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
+import {HotelType} from "../../backend/src/models/hotel.model"
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 //register
@@ -20,65 +22,75 @@ export const register = async (formData: RegisterFormData) => {
   }
 };
 
-
 //signin
-export const signIn = async(formData: SignInFormData) =>{
+export const signIn = async (formData: SignInFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
     method: "POST",
     credentials: "include",
-    headers:{
-      "Content-Type": "application/json"
+    headers: {
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(formData)
-  })
+    body: JSON.stringify(formData),
+  });
 
-  const body = await response.json()
+  const body = await response.json();
 
-  if(!response.ok){ 
+  if (!response.ok) {
     throw new Error(body.message);
   }
 
-  return body
-  
-}
-
-
+  return body;
+};
 
 //validate token
-export const validateToken = async () =>{
+export const validateToken = async () => {
   const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
-    credentials: "include"
-  })
-
-  if(!response.ok){
-    throw new Error("Token Invalid")
-  }
-
-  return response.json()
-}
-
-//signout
-export const signOut = async()=>{
-  const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
     credentials: "include",
-    method: "POST"
   });
 
-  if(!response.ok){
-    throw new Error ("Error in signing out")
+  if (!response.ok) {
+    throw new Error("Token Invalid");
   }
-}
 
-export const addMyHotel = async (hotelFormData: FormData)=>{
-  const response = await fetch (`${API_BASE_URL}/api/my-hotels`, {
+  return response.json();
+};
+
+//signout
+export const signOut = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+    credentials: "include",
+    method: "POST",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error in signing out");
+  }
+};
+
+// add hotel
+export const addMyHotel = async (hotelFormData: FormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
     method: "POST",
     credentials: "include",
-    body: hotelFormData
-  })
+    body: hotelFormData,
+  });
 
-  if(!response.ok){
-    throw new Error ("Failed to add new hotel");
+  if (!response.ok) {
+    throw new Error("Failed to add new hotel");
+  }
+
+  return response.json();
+};
+
+//get hotel
+export const fetchMyHotels = async (): Promise<HotelType> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
   }
 
   return response.json()
-}
+};
