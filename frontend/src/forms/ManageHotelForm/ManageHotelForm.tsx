4e94b29fc9ed8eb60f569daf4f4bab14253f30c7
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-04 12:27:12 +0530

    add-hotel functionality

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -19,17 +19,43 @@ export type HotelFormData = {
   childCount: number;
 };
 
-const ManageHotelForm = () => {
+type Props = {
+  onSave: (hotelFormData: FormData) => void;
+  isLaoding: boolean;
+};
+
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
   const formMethods = useForm<HotelFormData>();
   const { handleSubmit } = formMethods;
 
-  const onSubmit = handleSubmit((formData: HotelFormData) => {
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
     //create new formdata object and call our API
-    try {
-      console.log(formData);
-    } catch (error) {
-      console.log(error);
-    }
+    const formData = new FormData();
+    formData.append("name", formDataJson.name);
+    formData.append("country", formDataJson.country);
+    formData.append("city", formDataJson.city);
+    formData.append("description", formDataJson.description);
+    formData.append("type", formDataJson.type);
+    formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+    formData.append("starRating", formDataJson.starRating.toString());
+    formData.append("adultCount", formDataJson.adultCount.toString());
+    formData.append("childCount", formDataJson.childCount.toString());
+
+    formDataJson.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
+    });
+
+    Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    });
+
+    onSave(formData);
+
+    // try {
+    //   console.log(formData);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   });
 
   return (
@@ -44,10 +70,11 @@ const ManageHotelForm = () => {
         <div className="flex justify-center items-center mt-4">
           <span className="flex justify-center items-center w-[80%]">
             <button
+              disabled={isLoading}
               type="submit"
-              className="text-white text-sm md:text-lg  md:p-3  p-2 bg-purple-600 font-bold hover:bg-purple-500 rounded-lg mt-2 flex justify-center items-center w-full md:w-[500px] "
+              className="text-white text-sm md:text-lg  md:p-3  p-2 bg-purple-600 font-bold hover:bg-purple-500 rounded-lg mt-2 flex justify-center items-center w-full md:w-[500px] disabled:bg-gray-500"
             >
-              Save
+              {isLoading ? "Saving..." : "Save"}
             </button>
           </span>
         </div>
