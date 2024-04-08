984790da164dd240f2429c27d14a0c5f3779de78
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-08 07:52:59 +0530

    manage-hotels.specs test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -51,5 +51,21 @@ test("should allow user to add a hotel", async ({ page }) => {
 
   await page.getByRole("button", { name: "Save" }).click();
   await expect(page.getByText("Hotel Saved!")).toBeVisible();
+});
 
+test("should display hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+    await expect(page.getByText("test name")).toBeVisible();
+  await expect(page.getByText("test-test-test")).toBeVisible();
+
+  await expect(page.getByText("delhi, india")).toBeVisible();
+  await expect(page.getByText("Budget")).toBeVisible();
+  await expect(page.getByText("1000 per night")).toBeVisible();
+  await expect(page.getByText("3 adults, 1 children")).toBeVisible();
+  await expect(page.getByText("5 Star Rating")).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 });
