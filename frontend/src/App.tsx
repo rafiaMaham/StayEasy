7cf6154c3ae34e8047798adcf707a8d9e711aac8
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-09 14:18:31 +0530

    search page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -32,7 +33,7 @@ const App = () => {
           path="/search"
           element={
             <Layout>
-              <p>Search page</p>
+              <Search />
             </Layout>
           }
         />
