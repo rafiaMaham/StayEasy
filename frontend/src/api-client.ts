33b0bf36a427db9b75f20740a455b16cc9a58971
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-10 00:44:34 +0530

    added fucntionality of filter section  in frontend

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -135,25 +135,44 @@ export type SearchParams = {
   adultCount?: string;
   childCount?: string;
   page?: string;
+  facilities?: string[];
+  types?: string[];
+  stars?: string[];
+  maxPrice?: string;
+  sortOptions?: string;
 };
 
-export const searchHotels = async (searchParams: SearchParams): Promise<HotelSearchResponse> => {
+export const searchHotels = async (
+  searchParams: SearchParams
+): Promise<HotelSearchResponse> => {
   const queryParams = new URLSearchParams();
-  queryParams.append("destination", searchParams.destination || "")
-  queryParams.append("checkIn", searchParams.checkIn || "")
-  queryParams.append("checkOut", searchParams.checkOut|| "")
-  queryParams.append("adultCount", searchParams.adultCount || "")
-  queryParams.append("childCount", searchParams.childCount || "")
+  queryParams.append("destination", searchParams.destination || "");
+  queryParams.append("checkIn", searchParams.checkIn || "");
+  queryParams.append("checkOut", searchParams.checkOut || "");
+  queryParams.append("adultCount", searchParams.adultCount || "");
+  queryParams.append("childCount", searchParams.childCount || "");
   queryParams.append("page", searchParams.page || "");
 
+  queryParams.append("maxPrice", searchParams.maxPrice || "");
+  queryParams.append("sortOptions", searchParams.sortOptions || "");
+
+  searchParams.facilities?.forEach((facility) =>
+    queryParams.append("facilities", facility)
+  );
+
+  searchParams.types?.forEach((type) => queryParams.append("types", type));
+
+  searchParams.stars?.forEach((star) => queryParams.append("stars", star));
 
   //make fetch request
 
-  const response = await fetch (`${API_BASE_URL}/api/hotels/search?${queryParams}`)
+  const response = await fetch(
+    `${API_BASE_URL}/api/hotels/search?${queryParams}`
+  );
 
-  if(!response.ok){
-    throw new Error ("Error fetching hotels")
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
   }
 
-  return response.json()
+  return response.json();
 };
