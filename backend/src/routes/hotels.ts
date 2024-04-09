d70d7ef84a9aea98cc263dc148e297301125a85d
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-09 11:49:57 +0530

    search api route

---
+++ backend/src/routes/hotels.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import Hotel from "../models/hotel.model";
+import { HotelSearchResponse } from "../shared/types";
+
+const router = express.Router();
+
+// /api/hotels/search?
+
+router.get("/search", async (req: Request, res: Response) => {
+  try {
+    const pageSize = 5;
+    const pageNumber = parseInt(
+      req.query.page ? req.query.page.toString() : "1"
+    );
+
+    //pageNumber = 3, then skip = 10
+    const skip = (pageNumber - 1) * pageSize;
+
+    const hotels = await Hotel.find().skip(skip).limit(pageSize);
+
+    const total = await Hotel.countDocuments();
+
+    const response: HotelSearchResponse = {
+      data: hotels,
+      pagination: {
+        total,
+        page: pageNumber,
+        pages: Math.ceil(total / pageSize),
+      },
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+});
+
+export default router;
