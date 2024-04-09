bd868bc6786b0503bc98364b265e91979463a448
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-09 12:17:59 +0530

    SearchContextProvider

---
+++ frontend/src/contexts/SearchContext.tsx
@@ -0,0 +1,75 @@
+//this is going to be context which store the search criteria which is filled in the form once they hit submit
+
+import React, { useContext, useState } from "react";
+
+type SerachContext = {
+  destination: string;
+  checkIn: Date;
+  checkOut: Date;
+  adultCount: number;
+  childCount: number;
+  hotelId: string;
+  saveSearchValues: (
+    destination: string,
+    checkIn: Date,
+    checkOut: Date,
+    adultCount: number,
+    childCount: number
+  ) => void;
+};
+
+const SearchContext = React.createContext<SerachContext | undefined>(undefined); // defaulting is set to undefined bcs 1st time the app loads it is undefined
+
+type SearchcontextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const SearchContextProvider = ({
+  children,
+}: SearchcontextProviderProps) => {
+  const [destination, setDestination] = useState<string>("");
+  const [checkIn, setCheckIn] = useState<Date>(new Date());
+  const [checkOut, setCheckOut] = useState<Date>(new Date());
+  const [adultCount, setAdultCount] = useState<number>(1);
+  const [childCount, setChildCount] = useState<number>(0);
+  const [hotelId, setHotelId] = useState<string>("");
+
+  const saveSearchValues = (
+    destination: string,
+    checkIn: Date,
+    checkOut: Date,
+    adultCount: number,
+    childCount: number,
+    hotelId?: string
+  ) => {
+    setDestination(destination);
+    setCheckIn(checkIn);
+    setCheckOut(checkOut);
+    setAdultCount(adultCount);
+    setChildCount(childCount);
+    if (hotelId) {
+      setHotelId(hotelId);
+    }
+  };
+
+  return (
+    <SearchContext.Provider
+      value={{
+        destination,
+        checkIn,
+        checkOut,
+        adultCount,
+        childCount,
+        hotelId,
+        saveSearchValues,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearchContext = () => {
+  const context = useContext(SearchContext);
+  return context as SerachContext;
+};
