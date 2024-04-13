da0604b2fcf062901cd1803c7fafd884f1a08aae
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-13 11:47:50 +0530

    get logged in user route

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,8 +2,25 @@ import express, { Request, Response } from "express";
 import User from "../models/user.model";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
+
+//get logged in user
+router.get("/me", verifyToken, async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ meessage: "Something went wrong" });
+  }
+});
 
 // /api/users/register
 router.post(
