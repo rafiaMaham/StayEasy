8da025a87fa35f4f6ab54290b868f44885a972a8
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-04-13 13:46:17 +0530

    booking confirmation page

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,17 +1,61 @@
 import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
+import BookingForm from "../forms/BookingForm/BookingForm";
+import BookingDetailsSummary from "../components/BookingDetailsSummary";
+import { useSearchContext } from "../contexts/SearchContext";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Booking = () => {
-  //fetch current user endpoint using react query
+  //get search data from search context
+  const search = useSearchContext();
 
+  const { hotelId } = useParams();
+
+  const [numberOfNights, setNumberOfNights] = useState<number>(0);
+
+  useEffect(() => {
+    if (search.checkIn && search.checkOut) {
+      const nights =
+        Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+        (1000 * 60 * 60 * 24);
+
+      setNumberOfNights(Math.ceil(nights));
+    }
+  }, [search.checkIn, search.checkOut]);
+
+  //fetch hotels
+  const { data: hotel } = useQuery(
+    "fetchHotelByID",
+    () => apiClient.fetchMyHotelById(hotelId as string),
+    {
+      enabled: !!hotelId,
+    }
+  );
+  //fetch current user endpoint using react query
   const { data: currentUser } = useQuery(
     "fetchCurrentUser",
     apiClient.fetchCurrentUser
   );
 
-  console.log(currentUser?.email);
+ if(!hotel){
+    return<></>
+ }
 
-  return<></>
+  return (
+    <div className="grid md:grid-cols-[1fr_2fr]">
+      <BookingDetailsSummary
+        checkIn={search.checkIn}
+        checkOut={search.checkOut}
+        adultCount={search.adultCount}
+        childCount={search.childCount}
+        numberOfNights={numberOfNights}
+        hotel={hotel}
+      />
+
+      {currentUser && <BookingForm currentUser={currentUser} />}
+    </div>
+  );
 };
 
 export default Booking;
